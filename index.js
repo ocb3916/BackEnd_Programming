3c85d96eaa3b82e9659c8997fb9a1460bcd44c3d
ocb3916 <dhchanbin1004@gmail.com>
2024-03-12 15:17:54 +0900

    express 설치

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("노드js 프로젝트 시작!!")
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

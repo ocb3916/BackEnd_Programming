608d61bb35b1a9d463257e33dc8761d34c8c1c02
ocb3916 <dhchanbin1004@gmail.com>
2024-03-12 16:00:23 +0900

    express 설치

---
+++ lecture_note/111.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const async = require('async');
+
+fs.readFile('a.txt', (error, file) => {
+    console.log(file.toString());
+})
+
+fs.readFile('b.txt', (error, file) => {
+    console.log(file.toString());
+})
+
+fs.readFile('c.txt', (error, file) => {
+    console.log(file.toString());
+})
+
+//실행시켜보면 a,b,c 출력 순서가 계속 바뀌는 것을 확인할 수 있다.
+//비동기처리

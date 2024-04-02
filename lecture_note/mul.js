1cdf19388bbed0cca8c1ee91139072bb92c967bf
ocb3916 <dhchanbin1004@gmail.com>
2024-04-02 16:13:35 +0900

    .

--- lecture_note/mul.js
+++ lecture_note/mul.js
@@ -1,5 +1,17 @@
 function mul(a, b) {
-    return a * b;
+    let result = 1;
+    if (a < b) {
+        while (a <= b) {
+            result *= a;
+            a++;
+        }
+    }
+    else
+        while (a >= b) {
+            result *= a;
+            a--;
+        }
+    return result;
 }
 
 console.log(mul(5, 3));
--- lecture_note/sum.js
+++ lecture_note/sum.js
@@ -1,5 +1,17 @@
 function sum(a, b) {
-    return a + b;
+    let result = 0;
+    if (a < b) {
+        while (a <= b) {
+            result += a;
+            a++;
+        }
+    }
+    else
+        while(a >= b) {
+            result += a;
+            a--;
+        }
+    return result;
 }
 
 console.log(sum(5, 3));
